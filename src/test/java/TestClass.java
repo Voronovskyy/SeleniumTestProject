9a74c89a818e4593138da69dc2949ddf58d9d5bb
Danik Voronovsky <danik.voronovsky@gmail.com>
2022-02-03 16:12:57 +0200

    Added first test

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class TestClass extends BaseTest {
+
+    @Test
+    public void test() {
+        System.out.println("Chrome is started and page is opened");
+    }
+}
